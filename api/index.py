3439caae697eda766a71465838426a711cccd7b2
1yul2 <hanyul9824@gmail.com>
2025-11-10 13:56:11 +0900

    feat: resume page add

--- api/index.py
+++ api/index.py
@@ -1,6 +1,7 @@
 import os
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import FileResponse, HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from dotenv import load_dotenv
@@ -32,3 +33,16 @@ def contact(request: Request):
         "contact_email": os.getenv("CONTACT_EMAIL"),
 
     })
+
+@app.get("/resume", response_class=HTMLResponse)
+async def resume_page(request: Request):
+    return templates.TemplateResponse("resume.html", {"request": request, "error": None})
+
+@app.post("/resume", response_class=HTMLResponse)
+async def resume_download(request: Request, password: str = Form(...)):
+    correct_pw = os.getenv("RESUME_PASSWORD")
+    if password == correct_pw:
+        file_path = "api/static/resume.pdf"
+        return FileResponse(path=file_path, filename="Hanyul_Resume.pdf", media_type="application/pdf")
+    else:
+        return templates.TemplateResponse("resume.html", {"request": request, "error": "비밀번호가 올바르지 않습니다."})
